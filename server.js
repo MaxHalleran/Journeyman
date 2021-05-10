da7647a38060fc38b14a5859e5bcb023e0e5e2eb
MaxHalleran <maxhalleran@gmail.com>
2021-05-09 23:39:09 -0600

    Removed dotenv require

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
 const express = require("express");
 const app = express();
 
 const port = ( process.env.PORT ? ( process.env.PORT === 3001 ? 3000 : 3001 ) : 3001 );
 
 // Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || "development") {
   app.use(express.static("client/build"));
 } else {
   app.get('/', (req, res) => {
