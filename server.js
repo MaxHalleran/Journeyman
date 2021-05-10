f4e244555069eb6b1f5f456db996c93ee5795f3d
MaxHalleran <maxhalleran@gmail.com>
2021-05-09 23:24:25 -0600

    Updated environment to have local use live webpack server and live use built static files

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 
@@ -6,6 +7,10 @@ const port = ( process.env.PORT ? ( process.env.PORT === 3001 ? 3000 : 3001 ) : 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+} else {
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
 }
 
 app.use('/api', require('./routes/api/index'));
