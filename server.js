39af2546ea92023f458e58da336ee6b20671afbf
MaxHalleran <maxhalleran@gmail.com>
2021-05-09 20:21:24 -0600

    added api route

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.use('/api', require('./routes/api/index'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
