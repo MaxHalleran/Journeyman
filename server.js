c9966c22ea5c9278180bda8d33e5c834ab3bcebf
MaxHalleran <maxhalleran@gmail.com>
2021-05-09 23:51:01 -0600

    Removed port changing based on randomly assigned port environment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = ( process.env.PORT ? ( process.env.PORT === 3001 ? 3000 : 3001 ) : 3001 );
+const port = process.env.PORT || 3001;
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production" || "development") {
