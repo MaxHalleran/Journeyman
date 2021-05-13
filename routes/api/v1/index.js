1f46ef15dad6fd0a35fc6dcee8ff07a097b69adf
MaxHalleran <maxhalleran@gmail.com>
2021-05-12 19:13:50 -0600

    Added database

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
 
-router.get('/', (req, res) => {
-  res.send('This is the api route')
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+console.log(process.env.DATABASE_URL);
+
+router.get('/', (req, res) => {
+  res.send('This is the api route');
+});
+
+router.get('/db', async (req, res) =>  {
+  try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 3001;
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production" || "development") {
-  app.use(express.static("client/build"));
-} else {
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
-}
-
+// API endpoint routes
 app.use('/api', require('./routes/api/index'));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
+
+  // Client folder built
+  app.use(express.static("client/build"));
+
+} else {
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
