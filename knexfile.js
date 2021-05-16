c1ded8dba62453b4cbf12a8ba17805499e9f0b13
MaxHalleran <maxhalleran@gmail.com>
2021-05-16 14:38:54 -0600

    Updated production environment database configuration to both accept ssl lacking connections and set the database url

--- knexfile.js
+++ knexfile.js
@@ -36,19 +36,10 @@ module.exports = {
   // live environment
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
-    // connection: {
-    //   database: 'my_db',
-    //   user:     'username',
-    //   password: 'password'
-    // },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
     migrations: {
       directory: './data/migrations',
     },
